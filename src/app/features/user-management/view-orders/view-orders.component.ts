a3a75dca6d0bcb9bf7722db86624708c68783092
Kaushal P. Shah <shahkaus@pmc.local>
2020-03-24 19:14:09 +0530

    user order modified

--- src/app/features/user-management/view-orders/view-orders.component.ts
+++ src/app/features/user-management/view-orders/view-orders.component.ts
@@ -8,29 +8,82 @@ import { ConfirmDialogService } from '@shared/confirm-dialog/confirm-dialog.serv
   styleUrls: ['./view-orders.component.scss']
 })
 export class ViewOrdersComponent implements OnInit {
-  orderitems;
   heading = 'YOUR ORDERS';
-  constructor(private view: QuickViewService, private dialog: ConfirmDialogService) {}
+  i: number = 0;
+  orderitems: any;
+  displayedColumns: string[] = ['number', 'image', 'name', 'price', 'quantity', 'action'];
+  constructor(private view: QuickViewService, private dialog: ConfirmDialogService) { }
 
   ngOnInit() {
+
     this.orderitems = [
       {
-        name: 'tshirt',
-        price: 40000,
-        quantity: 6,
-        date: '8 May',
-        image:
-          'https://img.looksgud.com/upload/item-image/576/ccsy/ccsy-maniac-maniac-mens-fullsleeve-round-neck-dark-grey-cotton_500x500_1.jpg'
+        name: 'Order-1',
+        date: '23-May-2020',
+        products: [
+          {
+            name: 'tshirt',
+            price: 40000,
+            quantity: 6,
+            image:
+              'https://img.looksgud.com/upload/item-image/576/ccsy/ccsy-maniac-maniac-mens-fullsleeve-round-neck-dark-grey-cotton_500x500_1.jpg',
+            productqty: 50
+          },
+          {
+            name: 'tshirt',
+            price: 40000,
+            quantity: 6,
+            image:
+              'https://img.looksgud.com/upload/item-image/576/ccsy/ccsy-maniac-maniac-mens-fullsleeve-round-neck-dark-grey-cotton_500x500_1.jpg',
+            productqty: 50
+          },
+          {
+            name: 'tshirt',
+            price: 40000,
+            quantity: 6,
+            image:
+              'https://img.looksgud.com/upload/item-image/576/ccsy/ccsy-maniac-maniac-mens-fullsleeve-round-neck-dark-grey-cotton_500x500_1.jpg',
+            productqty: 50
+          }
+        ]
       },
       {
-        name: 'tshirt',
-        price: 40000,
-        quantity: 6,
-        date: '8 May',
-        image:
-          'https://img.looksgud.com/upload/item-image/576/ccsy/ccsy-maniac-maniac-mens-fullsleeve-round-neck-dark-grey-cotton_500x500_1.jpg'
-      }
-    ];
+        name: 'Order-2',
+        date: '23-May-2020',
+        products: [
+          {
+            name: 'tshirt',
+            price: 40000,
+            quantity: 6,
+            image:
+              'https://img.looksgud.com/upload/item-image/576/ccsy/ccsy-maniac-maniac-mens-fullsleeve-round-neck-dark-grey-cotton_500x500_1.jpg',
+            productqty: 50
+          }
+        ]
+      },
+      {
+        name: 'Order-3',
+        date: '23-May-2020',
+        products: [
+          {
+            name: 'tshirt',
+            price: 40000,
+            quantity: 6,
+            image:
+              'https://img.looksgud.com/upload/item-image/576/ccsy/ccsy-maniac-maniac-mens-fullsleeve-round-neck-dark-grey-cotton_500x500_1.jpg',
+            productqty: 50
+          },
+          {
+            name: 'tshirt',
+            price: 40000,
+            quantity: 6,
+            image:
+              'https://img.looksgud.com/upload/item-image/576/ccsy/ccsy-maniac-maniac-mens-fullsleeve-round-neck-dark-grey-cotton_500x500_1.jpg',
+            productqty: 50
+          }
+        ]
+      },
+    ]
   }
   quickview(item) {
     this.view.showQuickview(item);
@@ -43,5 +96,12 @@ export class ViewOrdersComponent implements OnInit {
           console.log('emptied');
         }
       });
+  }
+  reset() {
+    this.i = 0;
+  }
+  increment() {
+    this.i = this.i + 1;
+    return (this.i);
   }
 }
